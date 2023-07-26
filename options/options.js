4dc6c8ead53fe2256834d0d8cd49b89392e53713
Crowdbotics <engineering@crowdbotics.com>
2023-07-26 14:20:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-flower-de-104496",
+  url: "https://solitary-flower-de-104496.botics.co",
+  api: "https://solitary-flower-de-104496.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
